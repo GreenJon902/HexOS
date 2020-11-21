ea057de6d1098e6d840b008b7c254133a92eddfc
jj b <jj@thebabers.at>
2020-11-21 20:47:47 +0000

    more shit

--- globals.py
+++ globals.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
 sysConfig = ConfigParser()
-sysConfig.read("data/fixed_config_files/sysConfig.ini")
+sysConfig.read("data/fixed_config_files/sys_config.ini")
 
 path = ""
