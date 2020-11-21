96e11fbad5fe663f51dfac8217a253d28d1a9393
jj b <jj@thebabers.at>
2020-11-21 15:06:30 +0000

    got loggers working better maby
    I have no god dam idea

--- HexOS.py
+++ HexOS.py
@@ -1,4 +1,5 @@
 import os
+import time
 from configparser import ConfigParser
 
 from appdirs import user_data_dir
@@ -9,9 +10,10 @@ sysConfig = ConfigParser()
 sysConfig.read("sysConfig.ini")
 
 os.environ['KIVY_HOME'] = user_data_dir(sysConfig["main"]["name"])
-os.environ['KIVY_CONFIG_FILE'] = user_data_dir(sysConfig["main"]["name"])
+os.environ['KIVY_CONFIG_FILE'] = os.path.join(os.getcwd(), "kivyConfig.ini")
 os.environ[str(sysConfig["main"]["name"]) + '_CONFIG'] = str(sysConfig.__dict__)
 os.chdir(user_data_dir(sysConfig["main"]["name"]))
 
-HexOSLibs.setup_app()
-HexOSLibs.start_app()
+HexOSLibs.setup_os()
+HexOSLibs.start_os()
+
--- HexOSLibs/__init__.py
+++ HexOSLibs/__init__.py
@@ -1,11 +1,18 @@
 import os
 
 
-def setup_app():
+def setup_os():
     from kivy.config import Config
-    Config.read("kivyConfig.ini")
+    Config.read(os.environ['KIVY_CONFIG_FILE'])
     os.environ['KIVY_CONFIG'] = str(Config.__dict__)
 
+    for i in os.listdir("logs"):
+        if os.path.isfile(os.path.join(Config.get("kivy", "log_dir"), i)) and 'kivy' in i:
+            os.remove(os.path.join(Config.get("kivy", "log_dir"), i))
 
-def start_app():
+def start_os():
     import HexOSLibs.__main__
+
+
+__all__ = ["setup_os",
+           "start_os"]
